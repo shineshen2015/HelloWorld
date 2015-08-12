c1a2a3ced99969f387df3de1e0cb7ad4e4fb16ae
shineshen2015 <shineshen2015@sina.com>
2015-08-12 18:08:34 +0800

    commit 2th

---
+++ HelloWorld/src/com/shine/demo1/HelloWorld.java
@@ -0,0 +1,11 @@
+package com.shine.demo1;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("helloWorld£¡");
+
+	}
+
+}
